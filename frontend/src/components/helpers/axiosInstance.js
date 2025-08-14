5117478b22e8fa19b24f927bb6dd3208342791ad
Shivam Pandey <136367737+max925001@users.noreply.github.com>
2025-08-14 13:30:03 +0530

    Update axiosInstance.js

--- frontend/src/components/helpers/axiosInstance.js
+++ frontend/src/components/helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5001/api/"; // Adjust to your backend URL; use https://connectsphere-1.onrender.com/api/ if deployed
+const BASE_URL = "https://expense-tracker-ti1l.onrender.com/api/"; // Adjust to your backend URL; use https://connectsphere-1.onrender.com/api/ if deployed
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
